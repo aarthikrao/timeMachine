d846cdf34a0468e8e7d067fbac01e93692098771
Aarthik Rao <aaarthik@gmail.com>
2023-04-07 19:17:18 +0530

    add: grpc client

--- components/network/client.go
+++ components/network/client.go
@@ -1,10 +1,20 @@
 package network
 
-import "google.golang.org/grpc"
+import (
+	"context"
+
+	"github.com/aarthikrao/timeMachine/components/jobstore"
+	"google.golang.org/grpc"
+
+	jm "github.com/aarthikrao/timeMachine/models/jobmodels"
+)
 
 type networkHandler struct {
 	client JobStoreClient
 }
+
+// Compile time interface validation
+var _ jobstore.JobStore = &networkHandler{}
 
 func CreateConnection(addr string) (*networkHandler, error) {
 	conn, err := grpc.Dial(addr,
@@ -20,3 +30,42 @@ func CreateConnection(addr string) (*networkHandler, error) {
 	}, nil
 }
 
+func (nh *networkHandler) GetJob(collection, jobID string) (*jm.Job, error) {
+	resp, err := nh.client.GetJob(context.Background(), &jm.JobFetchDetails{
+		ID:         jobID,
+		Collection: collection,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &jm.Job{
+		TriggerTime: int(resp.TriggerTime),
+		ID:          resp.ID,
+		Meta:        resp.Meta,
+		Route:       resp.Route,
+	}, nil
+
+}
+
+func (nh *networkHandler) SetJob(collection string, job *jm.Job) error {
+	_, err := nh.client.SetJob(context.Background(), &jm.JobCreationDetails{
+		TriggerTime: int64(job.TriggerTime),
+		ID:          job.ID,
+		Meta:        job.Meta,
+		Route:       job.Route,
+		Collection:  collection,
+	})
+
+	return err
+}
+
+func (nh *networkHandler) DeleteJob(collection, jobID string) error {
+	_, err := nh.client.DeleteJob(context.Background(), &jm.JobFetchDetails{
+		Collection: collection,
+		ID:         jobID,
+	})
+
+	return err
+}
