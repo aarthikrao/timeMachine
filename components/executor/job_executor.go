88605a9c10e2cd5715716b77fdfd83324bccac58
Devansh Gupta <devanshguptamrt@gmail.com>
2023-07-30 00:17:48 +0530

    added: code for happy flow

--- components/executor/job_executor.go
+++ components/executor/job_executor.go
@@ -1,18 +1,54 @@
 package executor
 
 import (
+	"bytes"
+	"log"
+	"net/http"
+	"time"
+
 	"github.com/aarthikrao/timeMachine/models/jobmodels"
+	"go.uber.org/zap"
 )
 
+const defaultRequestContentType = "application/json"
+
 type executor struct {
+	logger *zap.Logger
 }
 
 func NewJobExecutor() Executor {
-	return &executor{}
+	devLogger, err := zap.NewDevelopment()
+	if err != nil {
+		log.Fatal("unable to get executor", zap.Error(err))
+	}
+	return &executor{
+
+		logger: devLogger,
+	}
 }
 
 func (exe *executor) Run(job jobmodels.Job) error {
+	if len(job.Route) == 0 {
+		exe.logger.Debug("return: no route")
+		return nil
+	}
 
+	triggerTime := time.UnixMilli(job.TriggerMS)
+	time.AfterFunc(time.Until(triggerTime), func() {
+		exe.makeHttpRequest(job)
+	})
 	return nil
 
+}
+
+func (exe *executor) makeHttpRequest(job jobmodels.Job) {
+	exe.logger.Debug("request recieved to execute", zap.Any("job", job))
+	_, err := http.Post(job.Route, defaultRequestContentType, bytes.NewReader(job.Meta))
+	if err != nil {
+		exe.logger.Error("error while posting job content",
+			zap.Any("job", job),
+			zap.Error(err))
+		return
+	}
+	exe.logger.Debug("done with makeing post call", zap.Any("job", job))
 }
