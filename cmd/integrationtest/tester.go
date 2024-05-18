805ba6f77b3c31f9403f8a6c0b54fbe64e93f8cc
Aarthik Rao <aaarthik@gmail.com>
2024-05-18 19:49:05 +0530

    Added: integration test

---
+++ cmd/integrationtest/tester.go
@@ -0,0 +1,124 @@
+// This package contains tools for basic testing of time machine DB
+//
+// It contains a client that publishes jobs to the time machine DB,
+// and a server which listens to the callback even
+//
+// Usage: go run cmd/tester/tester.go -c 10
+// Usage: ./tester -c 10
+// This will create 10 jobs in the time machine DB
+//
+// The server listens on port 4000 for callbacks
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/aarthikrao/timeMachine/models/jobmodels"
+	"github.com/aarthikrao/timeMachine/models/routemodels"
+	"github.com/aarthikrao/timeMachine/utils/httpclient"
+	timeUtils "github.com/aarthikrao/timeMachine/utils/time"
+)
+
+var (
+	// Route ID for this service
+	TesterRoute = "TesterRoute"
+
+	// URLs
+	JobURL   = "http://localhost:8001/job/test"
+	RouteURL = "http://localhost:8001/route"
+)
+
+type Tester struct {
+	client *httpclient.HTTPClient
+}
+
+func main() {
+	count := flag.Int("c", 0, "number of jobs to create")
+	flag.Parse()
+
+	t := &Tester{
+		client: httpclient.NewHTTPClient(30*time.Second, 10),
+	}
+
+	go func() {
+		http.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
+			// Handle callback logic here
+			var payload map[string]interface{}
+			err := json.NewDecoder(r.Body).Decode(&payload)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+			fmt.Println("Recieved callback:", payload)
+		})
+
+		err := http.ListenAndServe(":4000", nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	// Create a route
+	t.createRoute()
+
+	// Create jobs
+	t.createJobs(*count)
+
+	fmt.Println("Listening for callbacks on port 4000... Press Ctrl + C to exit")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("Exiting...")
+}
+
+func (t *Tester) createRoute() {
+	// Create a route
+	route := routemodels.Route{
+		ID:         TesterRoute,
+		Type:       routemodels.REST,
+		WebhookURL: "http://localhost:4000/callback",
+	}
+
+	by, statusCode, err := t.client.Post(RouteURL, route)
+	if err != nil {
+		fmt.Println("Error creating route:", err)
+		return
+	}
+	if statusCode != http.StatusOK {
+		fmt.Println("Error creating route. Status code:", statusCode, string(by))
+		return
+	}
+	fmt.Println("Created route:", string(by))
+
+}
+
+func (t *Tester) createJobs(count int) {
+	for i := 0; i < count; i++ {
+		var job jobmodels.Job = jobmodels.Job{
+			ID:        fmt.Sprintf("job-%d", i),
+			TriggerMS: timeUtils.GetCurrentMillis() + 61000,
+			Meta:      []byte(`{"foo": "bar"}`),
+			Route:     TesterRoute,
+		}
+
+		by, statusCode, err := t.client.Post(JobURL, job)
+		if err != nil {
+			fmt.Println("Error creating job:", err)
+			continue
+		}
+		if statusCode != http.StatusOK {
+			fmt.Println("Error creating job. Status code:", statusCode, string(by))
+			continue
+		}
+		fmt.Println("Created job", i, " : ", string(by))
+	}
+	fmt.Printf("Created %d jobs...\n", count)
+}
